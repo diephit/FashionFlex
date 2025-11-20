45f047c62a6f7999e0559f85a636b0e55b2dd2ad
khangndhe186523 <khangndhe186523@fpt.edu.vn>
2025-11-20 18:19:24 +0700

    Fix header

--- src/main/java/g6/fashionFlex/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package g6.fashionFlex.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/g6/fashionFlex/exception/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package g6.fashionFlex.exception;
-
-public class UserAlreadyExistsException extends RuntimeException {
-
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
